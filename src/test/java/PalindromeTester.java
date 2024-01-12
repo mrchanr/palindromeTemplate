88a4eadf535df18a5cf1b6a7b6823f303f8322f6
mrchanr <42151767+mrchanr@users.noreply.github.com>
2024-01-12 14:40:36 -0800

    Update and rename BillboardTester.java to PalindromeTester.java

---
+++ src/test/java/PalindromeTester.java
@@ -0,0 +1,32 @@
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.*;
+
+public class PalindromeTester {
+
+   @Test
+   public void sentences () {
+     PalindromeChecker sim = new PalindromeChecker();
+     assertEquals(true, sim.palindrome("Madam, I'm Adam!"));
+     assertEquals(true, sim.palindrome("A Man! A Plan! A Canal! Panama!"));
+   }
+   @Test
+   public void space(){
+      PalindromeChecker sim = new PalindromeChecker();
+      assertEquals(true, sim.palindrome("nurses run"));
+   }
+
+   @Test
+   public void word(){
+        assertEquals(false, sim.palindrome("test"));
+        assertEquals(true, sim.palindrome("rotator"));
+        assertEquals(false, sim.palindrome("rewriter"));
+   }
+}
